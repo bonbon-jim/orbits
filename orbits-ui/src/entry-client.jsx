b082495f97ded601e6f5e0d34206fade813406bf
Jinmin Fan <jim.jinmin.fan@outlook.com>
2025-09-11 16:44:49 +0800

    staging ..

--- orbits-ui/src/entry-client.jsx
+++ orbits-ui/src/entry-client.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.hydrateRoot(
   document.getElementById('root'),
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
--- orbits-ui/src/entry-server.jsx
+++ orbits-ui/src/entry-server.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { StaticRouter } from 'react-router-dom';
 import App from './App';
 
 export function render(url) {
   return ReactDOMServer.renderToString(
     <React.StrictMode>
-      <App />
+      <StaticRouter>
+        <App />
+      </StaticRouter>
     </React.StrictMode>
   );
 }
